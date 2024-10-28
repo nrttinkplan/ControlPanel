5ffffac91fc1a7b096ebe3ddc3b8c1ab8261e044
nrttinkplan <erhannurettinkplan14@gmail.com>
2024-10-28 17:19:46 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,186 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Svg, { Path } from 'react-native-svg';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const AnaSayfa = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <LinearGradient
+      colors={['rgba(248, 140, 140, 0.61)', '#000000', '#514E4E', 'rgba(0, 0, 0, 0)']}
+      locations={[0, 0.9998, 0.9999, 1]}
+      style={styles.container}
+    >
+      <View style={styles.bedroomButtonContainer}>
+        <TouchableOpacity>
+          <LinearGradient
+            colors={['#FDA43C', '#FFFFFF']}
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 0}}
+            style={styles.buttonBedroom}
+          >
+            <Text style={styles.buttonBedroomText}>Bedroom</Text>
+            <View style={styles.arrowContainer}>
+              <Svg width="14" height="7" viewBox="0 0 14 7">
+                <Path
+                  d="M1 1L7 6L13 1"
+                  stroke="#FF0000"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                />
+              </Svg>
+            </View>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.pervaneContainer}>
+        <Image
+          source={require('./assets/pervane.png')}
+          style={styles.pervane}
+        />
+        <Text style={styles.pervaneName}>Black Ceiling Fan</Text>
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.icon}>~</Text>
+          <Text style={styles.number}>20</Text>
+          <Text style={styles.icon}>≈</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.powerButtonContainer}>
+        <View style={styles.powerButtonWrapper}>
+          <Text style={styles.speedText}>Low</Text>
+          <View style={styles.powerButton}>
+            <Image
+              source={require('./assets/btn.png')}
+              style={styles.powerButtonBackground}
+            />
+            <Image
+              source={require('./assets/onOff.png')}
+              style={styles.powerButtonIcon}
+            />
+          </View>
+          <Text style={styles.speedText}>High</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </LinearGradient>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  bedroomButtonContainer: {
+    position: 'absolute',
+    top: 46,
+    right: 20,
+    zIndex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
+  buttonBedroom: {
+    width: 166,
+    height: 57,
+    borderRadius: 28.5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  buttonBedroomText: {
+    color: '#000000',
+    fontSize: 16,
+    marginRight: 8,
+  },
+  arrowContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 2,
+  },
+  pervaneContainer: {
+    position: 'absolute',
+    width: 375,
+    height: 195,
+    top: 133,
+    left: 21,
+    alignItems: 'center',
+  },
+  pervane: {},
+  pervaneName: {
+    marginTop: 35,
+    fontFamily: 'Roboto',
+    fontSize: 26,
+    lineHeight: 30.47,
+    color: '#FDA43C',
+    textAlign: 'center',
+  },
+  button: {
+    width: 245,
+    height: 77,
+    backgroundColor: '#FDA43C17',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 67,
+    borderTopRightRadius: 67,
+    borderBottomRightRadius: 67,
+    opacity: 0.9,
+    marginTop: 100,
+  },
+  icon: {
+    fontSize: 44,
+    color: '#FFAA33',
+    marginHorizontal: 52,
+  },
+  number: {
+    fontSize: 46,
+    fontWeight: 'bold',
+    color: '#FFAA33',
+  },
+  powerButtonContainer: {
+    position: 'absolute',
+    bottom: 50,
+    alignItems: 'center',
+    width: '100%',
+  },
+  powerButtonWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  powerButton: {
+    width: 200,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  powerButtonBackground: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    resizeMode: 'contain',
+  },
+  powerButtonIcon: {
+    width: '50%',
+    height: '50%',
+    resizeMode: 'contain',
+  },
+  speedText: {
+    color: '#FDA43C',
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    fontWeight: '500',
   },
 });
 
-export default App;
+export default AnaSayfa;
